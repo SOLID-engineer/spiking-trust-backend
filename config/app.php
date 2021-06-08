c9ea95c9b1f80869ad4e2bd09ba435436c0fff09
Son Nguyen <sono.online@gmail.com>
2021-06-08 14:11:59 +0700

    Added docker config file

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Emadadly\LaravelUuid\LaravelUuidServiceProvider::class,
         /*
          * Package Service Providers...
          */
