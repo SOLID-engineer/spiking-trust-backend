859b6f95d69532fbc87cf392df2253586dabe7c0
kiendt <dangtrungkien96@gmail.com>
2021-06-22 17:42:55 +0700

    template mail

---
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Helpers\PaginateFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $perPage = $request->input('prePage', 20);
+
+        $users = User::with('companies')
+                        ->orderByDesc('created_at')
+                        ->paginate($perPage);
+
+        /** @var LengthAwarePaginator $users */
+        $results = PaginateFormatter::format($users);
+
+        return response()->json($results, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function destroy($id, Request $request)
+    {
+        $user = User::find($id);
+        return response()->json($user, 200);
+    }
+}
